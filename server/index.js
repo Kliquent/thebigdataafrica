069b44eb05fc61a05714bee59c66dc163ec76ced
allistermugaisi <allistermugaisi@gmail.com>
2022-11-21 08:33:08 +0300

    test env port at dockerbuild

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	.then(() => console.log('MongoDB connected successfully!GMO'))
+	.then(() => console.log('MongoDB connected successfully!PORT.'))
 	.catch((error) => console.log(error));
 app.use(cors('*'));
 app.use(express.json()); // used to parse JSON bodies
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to TheBigDataAfrica api endpoint!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server is listening on *:${PORT}`));
