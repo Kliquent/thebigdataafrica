d16c3997c67159ac26e06c7190632780c53f7c10
allistermugaisi <allistermugaisi@gmail.com>
2022-11-27 19:47:13 +0300

    bug fix on admin deleteUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -24,10 +24,10 @@ router.post('/signin', signin);
 router.post('/user/signup', userSignup);
 router.post('/admin/signup', adminSignup);
 router.put('/admin/update-user-info', auth, adminUpdateUserInfo);
-router.put('/admin/delete-user', auth, adminDeleteUser);
 router.put('/update-user-info', auth, updateUserInfo);
 router.put('/update-password', auth, changePassword);
 router.put('/forgot-password', forgotPassword);
+router.delete('/admin/delete-user', auth, adminDeleteUser);
 router.get('/users-by-role/:roleId', auth, getUsersByRole);
 
 export default router;
