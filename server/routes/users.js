8593956cb3526bd001e1351ff1c5f40626f29c34
allistermugaisi <allistermugaisi@gmail.com>
2022-11-27 12:02:33 +0300

    get users by role api endpoint bug fix

--- server/routes/users.js
+++ server/routes/users.js
@@ -24,6 +24,6 @@ router.post('/admin/signup', adminSignup);
 router.put('/update-user-info', auth, updateUserInfo);
 router.put('/update-password', auth, changePassword);
 router.put('/forgot-password', forgotPassword);
-router.get(':roleId', auth, getUsersByRole);
+router.get('/users-by-role/:roleId', auth, getUsersByRole);
 
 export default router;
