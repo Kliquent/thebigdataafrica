130630a52ab1c04e8d7e6024689690c91a122b15
allistermugaisi <allistermugaisi@gmail.com>
2022-11-27 15:26:25 +0300

    removed view button

--- client/src/components/app/Home.jsx
+++ client/src/components/app/Home.jsx
@@ -475,34 +475,6 @@ const Home = () => {
 													<div className="flex items-center justify-end text-right">
 														<div className="p-2 cursor-pointer text-gray-400 hover:text-green-600">
 															<IconButton
-															// onClick={(e) => handleViewPopup(productType, e)}
-															>
-																<svg
-																	className="w-6 h-6"
-																	fill="none"
-																	stroke="currentColor"
-																	viewBox="0 0 24 24"
-																	xmlns="http://www.w3.org/2000/svg"
-																>
-																	<path
-																		strokeLinecap="round"
-																		strokeLinejoin="round"
-																		strokeWidth={2}
-																		height="1em"
-																		width="1em"
-																		d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-																	/>
-																	<path
-																		strokeLinecap="round"
-																		strokeLinejoin="round"
-																		strokeWidth={2}
-																		d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-																	/>
-																</svg>
-															</IconButton>
-														</div>
-														<div className="p-2 cursor-pointer text-gray-400 hover:text-green-600">
-															<IconButton
 																onClick={(e) => handleEditPopup(survey, e)}
 															>
 																<svg
