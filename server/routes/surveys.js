53c4c06519ce5634af9257264b563a1f6f53e715
allistermugaisi <allistermugaisi@gmail.com>
2022-11-28 11:35:01 +0300

    added url param

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -13,8 +13,8 @@ import { auth } from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/create', auth, createSurvey);
-router.get('/get-questions-by-survey', auth, getQuestionsBySurvey);
 router.get('/', auth, getSurveys);
+router.get('/get-questions-by-survey/:surveyId', auth, getQuestionsBySurvey);
 router.get('/:surveyId', auth, getSurvey);
 router.put('/:surveyId', auth, updateSurvey);
 router.delete('/:surveyId', auth, deleteSurvey);
