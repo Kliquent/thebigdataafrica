0d4ba5b56b6edd209fcc760cbf21a01c61a77b5e
allistermugaisi <allistermugaisi@gmail.com>
2022-12-27 13:02:27 +0300

    populated role_id on profile

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -3,7 +3,7 @@ import User from '../models/Users.js';
 export const profile = async (req, res) => {
 	const id = req.userId;
 	try {
-		const currentUser = await User.findById(id);
+		const currentUser = await User.findById(id).populate('role_id', 'title');
 
 		if (!currentUser)
 			return res.status(403).json({ message: 'No user found.' });
